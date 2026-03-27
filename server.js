2800d59d6ed8d643c124f3bc5834a5cd5273d0f1
Juan Antonio <sitoexpositorodriguez@gmail.com>
2026-03-27 13:43:41 +0100

    fix: serve index.html on root /

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/public', express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 // Privacy & Terms pages (Apple requires accessible URL)
 app.get('/privacy', (req, res) => res.sendFile(path.join(__dirname, 'public/privacy.html')));
